0903d0232c8aaf9c023a73eb05ba227b3107115c
LennartSchwier <lennart.schwier@icloud.com>
2022-12-07 18:40:41 +0100

    Refactor data modules

---
+++ data.tf
@@ -0,0 +1,30 @@
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_availability_zones" "availability_zones" {
+  state = "available"
+}
+
+data "aws_ami" "amazon_linux_2_kernel_5" {
+  owners = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0*x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
