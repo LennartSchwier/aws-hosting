7804ad8751a578ec144daca2f68279ab426218ca
LennartSchwier <lennart.schwier@icloud.com>
2022-12-07 11:55:42 +0100

    Add application load balancer

---
+++ elastic_load_balancer.tf
@@ -0,0 +1,24 @@
+resource "aws_lb_target_group" "lb_target_group" {
+  name = "cloud-programming-tg"
+  protocol = "HTTP"
+  port = "80"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_lb" "alb" {
+  name = "cloud-programming-lb"
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.http_port80.id]
+  subnets = data.aws_subnets.subnets.ids
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.id
+  port = 80
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.id
+  }
+}
--- launch_template.tf
+++ launch_template.tf
@@ -1,23 +1,3 @@
-data "aws_ami" "amazon_linux_2_kernel_5" {
-  owners = ["amazon"]
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0*x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
 resource "aws_launch_template" "launch_template" {
   name_prefix = "cloud-programming-launch-config-"
   image_id = data.aws_ami.amazon_linux_2_kernel_5.id
