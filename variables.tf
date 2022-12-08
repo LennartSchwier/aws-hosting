bc1935c2966f52e19ef13b8dca5e5d92d04ae5d8
LennartSchwier <lennart.schwier@icloud.com>
2022-12-08 11:10:20 +0100

    Add vpc and igw resources

--- variables.tf
+++ variables.tf
@@ -21,3 +21,9 @@ variable "vpc_id" {
   type = string
   default = "vpc-cce757a6"
 }
+
+variable "cidr_block" {
+  description = "cidr block assigned to vpc"
+  type = string
+  default = "172.31.0.0/16"
+}
