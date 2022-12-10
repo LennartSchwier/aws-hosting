002a2db815069fe2b7184da4c5808cd512d6c416
LennartSchwier <lennart.schwier@icloud.com>
2022-12-10 14:35:57 +0100

    Removed default values from variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "region" {
   description = "region for AWS resources"
   type = string
-  default = "eu-central-1"
 }
 
 variable "instance_type" {
   description = "type of instance"
   type = string
-  default = "t2.micro"
 }
